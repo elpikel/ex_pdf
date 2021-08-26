03f7e77c9682b11cd55fd9711b2b92e478b3c11d
Arkadiusz Plichta <arkadiusz@blockfi.com>
2021-08-26 16:56:43 +0200

    init

---
+++ lib/ex_pdf.ex
@@ -0,0 +1,18 @@
+defmodule ExPdf do
+  @moduledoc """
+  Documentation for `ExPdf`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExPdf.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_pdf_test.exs
@@ -0,0 +1,8 @@
+defmodule ExPdfTest do
+  use ExUnit.Case
+  doctest ExPdf
+
+  test "greets the world" do
+    assert ExPdf.hello() == :world
+  end
+end
